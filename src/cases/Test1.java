f590ee2b869f87c0c7ad3785dde682630a128fdb
pinkeypatel <charu.patel2015@gmail.com>
2022-01-04 16:28:07 -0600

    forexe

--- src/cases/Test1.java
+++ src/cases/Test1.java
@@ -2,17 +2,20 @@ package cases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class Test1 {
 public WebDriver driver;
 	
 	@Test(priority = 1)
-	public void op_driver() throws Exception{
-
+	public void test1() throws Exception{
+		System.out.println("Test case running");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
 		System.setProperty("webdriver.chrome.driver","./driver//chromedriver.exe");
 				
-				driver = new ChromeDriver();
+				driver = new ChromeDriver(options);
 				driver.manage().window().maximize();				
 				driver.get("https://www.facebook.com/");
 				Thread.sleep(2000);
